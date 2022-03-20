d7d79ebec1ac784e5edcb91691a9adbd796a6b33
Maru32768 <maru043@docomo.ne.jp>
2022-03-21 07:16:51 +0900

    remove NotNull

--- src/main/java/net/kunmc/lab/commandlib/annotation/NotNull.java
+++
@@ -1,9 +0,0 @@
-package net.kunmc.lab.commandlib.annotation;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.CLASS)
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
-public @interface NotNull {
-}
