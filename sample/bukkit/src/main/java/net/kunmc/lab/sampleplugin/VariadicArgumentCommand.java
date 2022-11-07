ff6e25543e17bb0866d69af92e4eaa13711a113e
Maru32768 <maru32768@gmail.com>
2022-11-07 09:27:05 +0900

    update samples

---
+++ sample/bukkit/src/main/java/net/kunmc/lab/sampleplugin/VariadicArgumentCommand.java
@@ -0,0 +1,23 @@
+package net.kunmc.lab.sampleplugin;
+
+import net.kunmc.lab.commandlib.Command;
+import net.kunmc.lab.commandlib.argument.BooleanArgument;
+import net.kunmc.lab.commandlib.argument.IntegerArgument;
+
+public class VariadicArgumentCommand extends Command {
+    public VariadicArgumentCommand() {
+        super("variadic");
+
+        argument(new IntegerArgument("number"),
+                (number, ctx) -> {
+                    ctx.sendSuccess(number);
+                });
+
+        argument(new IntegerArgument("number"),
+                new BooleanArgument("bool"),
+                (number, bool, ctx) -> {
+                    ctx.sendSuccess(number);
+                    ctx.sendSuccess(bool);
+                });
+    }
+}
