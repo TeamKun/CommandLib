819f56840c4b42660c5adfc6a559e5e331677f1a
Maru32768 <maru32768@gmail.com>
2023-09-14 22:59:47 +0900

    style: 定数を大文字のスネークケースに変更

--- bukkit/src/main/java/net/kunmc/lab/commandlib/util/nms/NMSReflection.java
+++ bukkit/src/main/java/net/kunmc/lab/commandlib/util/nms/NMSReflection.java
@@ -10,33 +10,33 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 public class NMSReflection {
-    private static final String version;
-    private static final String minecraftPackagePrefix = "net.minecraft";
-    private static final String nmsPackagePrefix;
-    private static final String craftBukkitPackagePrefix;
-    private static final Map<String, Optional<Class<?>>> cache = new ConcurrentHashMap<>();
-    private static final ClassLoader classLoader = Bukkit.getServer()
-                                                         .getClass()
-                                                         .getClassLoader();
+    private static final String VERSION;
+    private static final String MINECRAFT_PACKAGE_PREFIX = "net.minecraft";
+    private static final String NMS_PACKAGE_PREFIX;
+    private static final String CRAFT_BUKKIT_PACKAGE_PREFIX;
+    private static final Map<String, Optional<Class<?>>> CACHE = new ConcurrentHashMap<>();
+    private static final ClassLoader CLASS_LOADER = Bukkit.getServer()
+                                                          .getClass()
+                                                          .getClassLoader();
 
     static {
-        version = Bukkit.getServer()
+        VERSION = Bukkit.getServer()
                         .getClass()
                         .getPackage()
                         .getName()
                         .split("\\.")[3];
-        nmsPackagePrefix = "net.minecraft.server." + version;
-        craftBukkitPackagePrefix = "org.bukkit.craftbukkit." + version;
+        NMS_PACKAGE_PREFIX = "net.minecraft.server." + VERSION;
+        CRAFT_BUKKIT_PACKAGE_PREFIX = "org.bukkit.craftbukkit." + VERSION;
     }
 
     public static Class<?> findMinecraftClass(String className, String... classNames) {
         return Stream.concat(Stream.of(className), Stream.of(classNames))
                      .map(x -> {
-                         Class<?> nmsClass = findClass(nmsPackagePrefix + "." + x).orElse(null);
+                         Class<?> nmsClass = findClass(NMS_PACKAGE_PREFIX + "." + x).orElse(null);
                          if (nmsClass != null) {
                              return nmsClass;
                          }
-                         return findClass(minecraftPackagePrefix + "." + x).orElse(null);
+                         return findClass(MINECRAFT_PACKAGE_PREFIX + "." + x).orElse(null);
                      })
                      .filter(Objects::nonNull)
                      .findFirst()
@@ -44,14 +44,14 @@ public class NMSReflection {
     }
 
     public static Class<?> findCraftBukkitClass(String className) {
-        return findClass(craftBukkitPackagePrefix + "." + className).orElseThrow(() -> new NMSClassNotFoundException(
+        return findClass(CRAFT_BUKKIT_PACKAGE_PREFIX + "." + className).orElseThrow(() -> new NMSClassNotFoundException(
                 className));
     }
 
     private static Optional<Class<?>> findClass(String className) {
-        return cache.computeIfAbsent(className, k -> {
+        return CACHE.computeIfAbsent(className, k -> {
             try {
-                return Optional.of(classLoader.loadClass(k));
+                return Optional.of(CLASS_LOADER.loadClass(k));
             } catch (ClassNotFoundException e) {
                 return Optional.empty();
             }
